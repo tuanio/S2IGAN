397b1e96972229610e38db8c7a768c5774eaa264
Nguyen Van Anh Tuan <nvatuan3@gmail.com>
2022-12-21 02:17:55 +0700

    revert

--- s2igan/sen/sed.py
+++ s2igan/sen/sed.py
@@ -74,14 +74,14 @@ class SpeechEncoder(nn.Module):
         # print(l)
         # print(mel_spec_len)
 
-        # packed = pack_padded_sequence(
-        #     cnn_out, l, batch_first=True, enforce_sorted=False
-        # )
-        # self.rnn.flatten_parameters()
-        # out, hidden_state = self.rnn(packed)
-        # out, seq_len = pad_packed_sequence(out, batch_first=True)
-        # pack input before RNN to reduce computing efforts
-        out, hidden_state = self.rnn(cnn_out)
+        packed = pack_padded_sequence(
+            cnn_out, l, batch_first=True, enforce_sorted=False
+        )
+        self.rnn.flatten_parameters()
+        out, hidden_state = self.rnn(packed)
+        out, seq_len = pad_packed_sequence(out, batch_first=True)
+        pack input before RNN to reduce computing efforts
+        # out, hidden_state = self.rnn(cnn_out)
 
         out, weights = self.self_attention(out, out, out)
         out = out.mean(dim=1)  # mean the time step
